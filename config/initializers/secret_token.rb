bb193fb36078d197a6515b20f47b58a5a0e4d540
Younghun Choi <choi0hun@gmail.com>
2011-03-30 23:27:49 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fingeroo::Application.config.secret_token = 'd099fbe0dde6bcbd76f06861c079662e7e2d5f0fc7bab6bab261c3f29d247d4af02c8102e71f045efea092d5ff85fe1341741675045b4fc0ccd0a02d74ef81f5'
